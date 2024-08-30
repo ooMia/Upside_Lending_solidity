0773e1c51e54f14d09b4a9ba670c4fac72870ac3
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-08-30 13:48:53 +0900

    [21/28] 🚧 WIP : temp impl liquidation

--- src/DreamAcademyLending.sol
+++ src/DreamAcademyLending.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 interface IPriceOracle {
     function getPrice(address token) external view returns (uint256);
@@ -15,7 +16,7 @@ interface ILending {
     function repay(address token, uint256 amount) external;
     function withdraw(address token, uint256 amount) external;
     function getAccruedSupplyAmount(address token) external view returns (uint256);
-    function liquidate(address token, address borrower) external;
+    function liquidate(address borrower, address token, uint256 amount) external;
 }
 /// 1 block 당 12sec 고정
 
@@ -27,17 +28,23 @@ contract DreamAcademyLending is ILending {
     uint256 constant OC_RATE = 175;
     mapping(address => uint256) internal _price;
 
-    struct Collateral {
-        mapping(address => uint256) owned;
-        mapping(address => uint256) locked;
+    struct Deposit {
+        uint256 amount;
+        uint256 blockNumber;
     }
 
-    mapping(address => Collateral) internal _balances;
+    struct Balance {
+        mapping(address => Deposit) _deposit;
+        mapping(address => uint256) locked;
+        uint256 borrowed;
+    }
+
+    mapping(address => Balance) internal _balances;
 
     modifier updateBalance(address token, int256 amount) {
         _;
-        mapping(address => uint256) storage owned = _balances[msg.sender].owned;
-        owned[token] = uint256(int256(owned[token]) + amount);
+        Deposit storage _deposit = _balances[msg.sender]._deposit[token];
+        _deposit.amount = uint256(int256(_deposit.amount) + amount);
     }
 
     modifier updatePrice(address token) {
@@ -65,19 +72,20 @@ contract DreamAcademyLending is ILending {
     }
 
     /// @dev 맡겨둔 ETH를 담보로 USDC를 빌립니다.
-    /// 200%의 과담보율을 적용합니다. 예를 들어, 1000 USDC를 빌리려면 2000 USDC의 가치에 해당하는 ETH를 담보로 제공해야 합니다.
+    /// OC_RATE%의 과담보율을 적용합니다. 예를 들어, OC_RATE가 1750이라면 1000 USDC를 빌릴 때, 1750 USDC의 가치에 해당하는 ETH를 담보로 제공해야 합니다.
     /// @param amount 빌릴 USDC의 양
     function borrow(address token, uint256 amount) external override updatePrice(token) {
-        Collateral storage balance = _balances[msg.sender];
+        Balance storage balance = _balances[msg.sender];
 
         uint256 valueToBorrow = amount * _price[token];
         uint256 valueNeededToBorrow = (valueToBorrow * OC_RATE) / 100;
         require(valueNeededToBorrow <= totalValueOwned());
 
-        balance.owned[NATIVE_ETH] -= valueNeededToBorrow / _price[NATIVE_ETH];
+        balance._deposit[NATIVE_ETH].amount -= valueNeededToBorrow / _price[NATIVE_ETH];
 
-        balance.owned[token] += valueToBorrow / _price[token];
+        balance._deposit[token].amount += valueToBorrow / _price[token];
         balance.locked[token] += valueNeededToBorrow - valueToBorrow;
+        balance.borrowed += amount;
 
         require(ERC20(token).transfer(msg.sender, amount));
     }
@@ -85,19 +93,23 @@ contract DreamAcademyLending is ILending {
     event Log(uint256 value);
 
     function totalValueOwned() internal view returns (uint256 totalValue) {
-        totalValue += _balances[msg.sender].owned[NATIVE_ETH] * _price[NATIVE_ETH];
-        // totalValue += _balances[msg.sender].owned[_usdc] * _price[_usdc];
+        totalValue += _balances[msg.sender]._deposit[NATIVE_ETH].amount * _price[NATIVE_ETH];
     }
 
     // TODO block number를 이용한 이자 계산
     function repay(address usdc, uint256 amount) external override {
-        _balances[msg.sender].locked[usdc] -= amount;
-        _balances[msg.sender].owned[NATIVE_ETH] += amount;
-        require(ERC20(usdc).transfer(msg.sender, amount));
+        Balance storage balance = _balances[msg.sender];
+        balance.borrowed -= amount;
+        require(ERC20(usdc).transferFrom(msg.sender, address(this), amount));
+
+        if (balance.borrowed == 0) {
+            require(ERC20(usdc).transfer(msg.sender, balance.locked[usdc]));
+            balance.locked[usdc] = 0;
+        }
     }
 
     function withdraw(address token, uint256 amount) external override updateBalance(token, -int256(amount)) {
-        require(_balances[msg.sender].owned[token] >= amount);
+        require(_balances[msg.sender]._deposit[token].amount >= amount);
 
         if (token == NATIVE_ETH) {
             (bool res,) = msg.sender.call{value: amount}("");
@@ -107,9 +119,29 @@ contract DreamAcademyLending is ILending {
         require(ERC20(token).transfer(msg.sender, amount));
     }
 
-    function getAccruedSupplyAmount(address usdc) external view override returns (uint256) {}
+    function getAccruedSupplyAmount(address usdc) external view override returns (uint256) {
+        Deposit storage _deposit = _balances[msg.sender]._deposit[usdc];
+        return (_deposit.amount * (block.number - _deposit.blockNumber)) / 100;
+    }
 
-    function liquidate(address usdc, address borrower) external override {}
+    /// @dev can liquidate the whole position when the borrowed amount is less than 100, otherwise only 25% can be liquidated at once.
+    function liquidate(address borrower, address usdc, uint256 amount) external override {
+        Balance storage balance = _balances[borrower];
+        amount = amount < 100 ? Math.min(amount, balance.locked[usdc]) : Math.min(amount, balance.locked[usdc] / 4);
+
+        emit Log(amount);
+        emit Log(_price[usdc]);
+        emit Log(balance.borrowed);
+
+        if (amount / _price[usdc] == 0) {}
+        balance.borrowed -= amount * _price[usdc];
+
+        require(ERC20(usdc).transferFrom(msg.sender, address(this), amount));
+        if (balance.borrowed == 0) {
+            require(ERC20(usdc).transfer(borrower, balance.locked[usdc] / _price[usdc]));
+            balance.locked[usdc] = 0;
+        }
+    }
 
     // receive() external payable {}
     // fallback() external payable {}
