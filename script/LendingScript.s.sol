b9e556b85843513949a4c24a0e8e71be83fdc1e2
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-09-07 02:12:22 +0900

    🛠️ Chore : reset

--- script/LendingScript.s.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
-
-import {Script} from "forge-std/Script.sol";
-
-contract LendingScript is Script {
-    constructor() {}
-    function run() external {}
-}
--- test/LendingTest.t.sol
+++ test/LendingTest.t.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.13; // compatible with 0.8.13
 
 import "forge-std/Test.sol";
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {DreamAcademyLending, IPriceOracle} from "src/DreamAcademyLending.sol";
+import "src/DreamAcademyLending.sol";
 
 contract CUSDC is ERC20 {
     constructor() ERC20("Circle Stable Coin", "USDC") {
@@ -51,11 +51,10 @@ contract Testx is Test {
         vm.deal(address(this), 10000000 ether);
         usdc = new CUSDC();
 
-        // TDOO 아래 setUp이 정상작동 할 수 있도록 여러분의 Lending Contract를 수정하세요.
         lending = new DreamAcademyLending(IPriceOracle(address(dreamOracle)), address(usdc));
         usdc.approve(address(lending), type(uint256).max);
 
-        lending.initializeLendingProtocol{value: 1}(address(usdc)); // set reserve ^__^
+        lending.initializeLendingProtocol{value: 1}(address(usdc));
 
         dreamOracle.setPrice(address(0x0), 1339 ether);
         dreamOracle.setPrice(address(usdc), 1 ether);
@@ -395,7 +394,7 @@ contract Testx is Test {
             // 2000 / (4000 - 1333) * 100 = 74.xxxx
             // LT = 75%
             (success,) = address(lending).call(
-                abi.encodeWithSelector(DreamAcademyLending.withdraw.selector, address(0x0), (1 ether * 1333) / 4000)
+                abi.encodeWithSelector(DreamAcademyLending.withdraw.selector, address(0x0), 1 ether * 1333 / 4000)
             );
             assertTrue(success);
         }
@@ -424,7 +423,7 @@ contract Testx is Test {
             // LT = 75%
             vm.roll(block.number + 1000);
             (success,) = address(lending).call(
-                abi.encodeWithSelector(DreamAcademyLending.withdraw.selector, address(0x0), (1 ether * 1333) / 4000)
+                abi.encodeWithSelector(DreamAcademyLending.withdraw.selector, address(0x0), 1 ether * 1333 / 4000)
             );
             assertFalse(success);
         }
@@ -464,11 +463,11 @@ contract Testx is Test {
         }
         vm.stopPrank();
 
-        vm.roll(block.number + ((86400 * 1000) / 12));
+        vm.roll(block.number + (86400 * 1000 / 12));
         vm.prank(user3);
         assertTrue(lending.getAccruedSupplyAmount(address(usdc)) / 1e18 == 30000792);
 
-        vm.roll(block.number + ((86400 * 500) / 12));
+        vm.roll(block.number + (86400 * 500 / 12));
         vm.prank(user3);
         assertTrue(lending.getAccruedSupplyAmount(address(usdc)) / 1e18 == 30001605);
 
@@ -513,7 +512,7 @@ contract Testx is Test {
         }
         vm.stopPrank();
 
-        vm.roll(block.number + ((86400 * 1000) / 12));
+        vm.roll(block.number + (86400 * 1000 / 12));
         vm.prank(user3);
         assertTrue(lending.getAccruedSupplyAmount(address(usdc)) / 1e18 == 30000792);
 
@@ -524,7 +523,7 @@ contract Testx is Test {
         lending.deposit(address(usdc), 10000000 ether);
         vm.stopPrank();
 
-        vm.roll(block.number + ((86400 * 500) / 12));
+        vm.roll(block.number + (86400 * 500 / 12));
         vm.prank(user3);
         uint256 a = lending.getAccruedSupplyAmount(address(usdc));
 
@@ -614,7 +613,7 @@ contract Testx is Test {
         }
         vm.stopPrank();
 
-        dreamOracle.setPrice(address(0x0), ((4000 * 66) / 100) * 1e18); // drop price to 66%
+        dreamOracle.setPrice(address(0x0), (4000 * 66 / 100) * 1e18); // drop price to 66%
         usdc.transfer(user3, 3000 ether);
 
         vm.startPrank(user3);
@@ -651,7 +650,7 @@ contract Testx is Test {
         }
         vm.stopPrank();
 
-        dreamOracle.setPrice(address(0x0), ((4000 * 66) / 100) * 1e18); // drop price to 66%
+        dreamOracle.setPrice(address(0x0), (4000 * 66 / 100) * 1e18); // drop price to 66%
         usdc.transfer(user3, 3000 ether);
 
         vm.startPrank(user3);
@@ -685,7 +684,7 @@ contract Testx is Test {
         }
         vm.stopPrank();
 
-        dreamOracle.setPrice(address(0x0), ((4000 * 66) / 100) * 1e18); // drop price to 66%
+        dreamOracle.setPrice(address(0x0), (4000 * 66 / 100) * 1e18); // drop price to 66%
         usdc.transfer(user3, 3000 ether);
 
         vm.startPrank(user3);
@@ -725,7 +724,7 @@ contract Testx is Test {
 
         supplySmallEtherDepositUser2();
 
-        dreamOracle.setPrice(address(0x0), ((4000 * 66) / 100) * 1e18); // drop price to 66%
+        dreamOracle.setPrice(address(0x0), (4000 * 66 / 100) * 1e18); // drop price to 66%
         usdc.transfer(user3, 3000 ether);
 
         vm.startPrank(user3);
@@ -760,7 +759,7 @@ contract Testx is Test {
         }
         vm.stopPrank();
 
-        dreamOracle.setPrice(address(0x0), ((4000 * 66) / 100) * 1e18); // drop Ether price to 66%
+        dreamOracle.setPrice(address(0x0), (4000 * 66 / 100) * 1e18); // drop Ether price to 66%
         dreamOracle.setPrice(address(usdc), 1e17); // drop USDC price to 0.1, 90% down
         usdc.transfer(user3, 3000 ether);
 
